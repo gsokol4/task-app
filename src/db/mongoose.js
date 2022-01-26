18f7275b69d3f70247e84c87b7193512eed5440c
Gabriel Sokol <gsokol4@gmail.com>
2022-01-25 21:37:39 -0600

    worked on retreving ides from urls

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,34 +3,22 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
+// const User = mongoose.model('Person', {
+//   name: {
+//     type: String
+//   },
+//   age: {
+//     type: Number
+//   }
+// })
 
-const User = mongoose.model('Person', {
-  name: {
-    type: String
-  },
-  age: {
-    type: Number
-  }
-})
+// const me = new User({
+//   name: 'Donotello',
+//   age: 25
+// })
 
-const me = new User({
-  name: 'Donotello',
-  age: 25
-})
-
-me.save().then(
-  () => { console.log(me) }
-).catch(
-  (error) => { console.log('error:', error) }
-)
+// me.save().then(
+//   () => { console.log(me) }
+// ).catch(
+//   (error) => { console.log('error:', error) }
+// )
--- src/index.js
+++ src/index.js
@@ -1,19 +1,83 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+// app.get('/', (req, res) => {
+//   res.send('yoink it works')
+// })
+
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.send(users)
+  }).catch(() => {
+    res.status(500).send()
+  })
+})
+
+app.get('/users/:id', (req, res) => {
+  console.log(req.params)
+  const id = req.params.id
+
+  User.findById(id).then((personObject) => {
+    if (!personObject) {
+      console.log('there is no person object')
+      return res.status(404).send()
+    }
+    res.send(personObject)
+  }).catch((e) => {
+    res.send(e)
+  })
+})
+
 app.post('/users', (req, res) => {
-  const person = new User({ name: 'doggo', email: 'doggo@gmail.com', password: 'IamAGoodBoi#3', age: 3 })
+  const person = new User(req.body)
 
   person.save().then(() => {
-    res.send(person)
-  }).catch(() => {
+    res.status(201).send(person)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
 
+app.get('/task', (req, res) => {
+  Task.find({}).then((tasks) => {
+    res.send(tasks)
+  }).catch((e) => {
+    res.status(500).send()
+  })
+})
+
+app.get('/task/:id', (req, res) => {
+  console.log(req.body)
+  const id = req.body.id
+
+  Task.findById(id).then(
+    (task) => {
+      if (!task) {
+        return res.send(404)
+      }
+      res.send(task)
+    }
+  ).catch(
+    (e) => {
+      res.status(500).send(e)
+    }
+  )
+})
+
+app.post('/task', (req, res) => {
+  const task = new Task(req.body)
+  console.log(task)
+  task.save().then(() => {
+    res.status(201).send(task)
+  }).catch((e) => {
+    res.status(400).send(e)
   })
 })
 
