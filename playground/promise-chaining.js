bced0f25eac556b27e4b8e3f5b008c58856fccbd
Gabriel Sokol <gsokol4@gmail.com>
2022-02-10 22:59:20 -0600

    added the ability to deleate by id

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,2 +1,14 @@
 require('../src/db/mongoose')
+let Task = require('../src/models/task')
 
+const deleteTaskById = (id) => {
+  Task.findOneAndDelete({ _id: id }, (e, doc) => {
+    if (e) {
+      console.log(e)
+    } else {
+      console.log(`successfully deleted ${doc}`)
+    }
+  })
+}
+
+deleteTaskById('61ee33d2ad62e458027c3108')
